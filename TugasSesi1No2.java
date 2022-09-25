0040b1451a42267185e3c52b3def6fe943f47baa
Reza-shar <113996651+Reza-shar@users.noreply.github.com>
2022-09-25 07:36:51 +0700

    Add files via upload

---
+++ TugasSesi1No2.java
@@ -0,0 +1,10 @@
+
+import javax.swing.JOptionPane;
+
+
+public class TugasSesi1No2 {
+    public static void main(String[] args) {
+        String inputan = JOptionPane.showInputDialog("Anda sedang belajar apa?");
+        JOptionPane.showMessageDialog(null,"Belajar" + inputan + "sangat mudah");
+    }
+}
---
+++ Tugas_sesi_1.java
@@ -0,0 +1,12 @@
+
+import javax.swing.JOptionPane;
+
+
+public class Tugas_sesi_1 {
+    public static void main(String[]args){
+        JOptionPane.showMessageDialog(null,"Belajar JAVA");
+        JOptionPane.showMessageDialog(null,"Sangat mudah sekali");
+        JOptionPane.showMessageDialog(null,"dan sangat menyenangkan");
+    }
+   
+}
